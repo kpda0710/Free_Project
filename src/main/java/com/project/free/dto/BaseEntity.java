9078b71b065eed46746ffd0aba730d6ceae1b9a4
kpda0710 <kpda0710@naver.com>
2025-01-18 21:58:38 +0900

    기본 엔티티 설정

---
+++ src/main/java/com/project/free/dto/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.project.free.dto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.MappedSuperclass;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@SuperBuilder
+@MappedSuperclass
+@AllArgsConstructor
+@NoArgsConstructor
+public abstract class BaseEntity {
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    private Boolean isDeleted = false;
+}
