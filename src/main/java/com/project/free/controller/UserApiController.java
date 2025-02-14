c8b89f28d0063073e0bc2038d46df1794e81310c
kpda0710 <kpda0710@naver.com>
2025-02-14 21:51:38 +0900

    커스텀 응답객체 유저 api 적용

--- src/main/java/com/project/free/controller/UserApiController.java
+++ src/main/java/com/project/free/controller/UserApiController.java
@@ -1,7 +1,9 @@
 package com.project.free.controller;
 
 import com.project.free.dto.user.*;
+import com.project.free.exception.ResponseCode;
 import com.project.free.service.UserService;
+import com.project.free.util.CustomResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -21,39 +23,39 @@ public class UserApiController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public ResponseEntity<UserResponse> signupUser(@RequestBody UserRequest userRequest) {
+    public CustomResponse<UserResponse> signupUser(@RequestBody UserRequest userRequest) {
         UserResponse userResponse = userService.signupUser(userRequest);
-        return ResponseEntity.status(HttpStatus.OK).body(userResponse);
+        return CustomResponse.success(ResponseCode.USER_CREATE_SUCCESS, userResponse);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> loginUser(@RequestBody UserLoginRequest userLoginRequest) {
+    public CustomResponse<String> loginUser(@RequestBody UserLoginRequest userLoginRequest) {
         String token = userService.loginUser(userLoginRequest);
-        return ResponseEntity.status(HttpStatus.OK).body(token);
+        return CustomResponse.success(ResponseCode.USER_LOGIN_SUCCESS, token);
     }
 
     @GetMapping()
-    public ResponseEntity<UserResponse> getUser(Authentication authentication) {
+    public CustomResponse<UserResponse> getUser(Authentication authentication) {
         UserResponse userResponse = userService.getUser(authentication);
-        return ResponseEntity.status(HttpStatus.OK).body(userResponse);
+        return CustomResponse.success(ResponseCode.USER_GET_SUCCESS, userResponse);
     }
 
     @PutMapping()
-    public ResponseEntity<UserResponse> updateUser(@RequestBody UserRequest userRequest, Authentication authentication) {
+    public CustomResponse<UserResponse> updateUser(@RequestBody UserRequest userRequest, Authentication authentication) {
         UserResponse userResponse = userService.updateUser(userRequest, authentication);
-        return ResponseEntity.status(HttpStatus.OK).body(userResponse);
+        return CustomResponse.success(ResponseCode.USER_UPDATE_SUCCESS, userResponse);
     }
 
     @DeleteMapping()
-    public ResponseEntity<Void> deleteUser(Authentication authentication) {
+    public CustomResponse<Void> deleteUser(Authentication authentication) {
         userService.deleteUser(authentication);
-        return ResponseEntity.status(HttpStatus.OK).body(null);
+        return CustomResponse.success(ResponseCode.USER_DELETE_SUCCESS, null);
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/all")
-    public ResponseEntity<List<UserResponse>> getAllUsers(Authentication authentication) {
+    public CustomResponse<List<UserResponse>> getAllUsers(Authentication authentication) {
         List<UserResponse> userResponseList = userService.getUserAll(authentication);
-        return ResponseEntity.status(HttpStatus.OK).body(userResponseList);
+        return CustomResponse.success(ResponseCode.SUCCESS, userResponseList);
     }
 }
